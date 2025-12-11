937da3b4487e0a1a1c1d6966e8d37bdf221b5bd8
Arthur <arthur.p31@aluno.ifsc.edu.br>
2025-12-11 15:27:57 -0300

    Most of the second level finished

--- Assets/KillZone.cs
+++ Assets/KillZone.cs
@@ -4,16 +4,11 @@ using UnityEngine;
 
 public class KillZone : MonoBehaviour
 {
-    public class ColliderOnTriggerMove : MonoBehaviour
-{
     private void OnTriggerEnter(Collider other)
     {
         if(GameManager.gm)
             {
-                GameManager.gm.finalizarPartida();
                 other.transform.position = GameManager.gm.lastPos;
             }
     }
-}
-
 }
