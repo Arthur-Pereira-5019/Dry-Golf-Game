b89bac88ee4447d478cba3faa90b7ea073ae0c61
Arthur <arthur.p31@aluno.ifsc.edu.br>
2025-12-11 14:25:49 -0300

    Finished most basic mechanics of the game

--- Assets/Buraco.cs
+++ Assets/Buraco.cs
@@ -4,14 +4,13 @@ using UnityEngine;
 
 public class Buraco : MonoBehaviour
 {
-    public static GameManager gm;
     private void OnCollisionEnter(Collision collision)
     {
         if(collision.gameObject.CompareTag("Player"))
         {
             if(GameManager.gm)
             {
-                gm.finalizarPartida();
+                GameManager.gm.finalizarPartida();
             }
         }
     }
--- Assets/GameManager.cs
+++ Assets/GameManager.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using JetBrains.Annotations;
 using TMPro;
 using Unity.VisualScripting;
 using UnityEngine;
@@ -10,8 +11,8 @@ using UnityEngine.SceneManagement;
 public class GameManager : MonoBehaviour
 {
     public int par;
-    public int tacadas;
-    public int pontuacao;
+    public int tacadas = 0;
+    public int pontuacao = 0;
 
     public Canvas canvaPadrao;
 
@@ -28,12 +29,27 @@ public class GameManager : MonoBehaviour
 
     public GameObject player;
 
+    public GameObject camera1;
+    public GameObject camera2;
 
+void Awake()
+{
+    if (gm != null && gm != this)
+    {
+        Destroy(gameObject);
+        return;
+    }
+    gm = this;
+    DontDestroyOnLoad(gameObject);
+}
     public int mapa;
     void Start()
     {
+        camera2.SetActive(false);
         canvaPadrao.enabled = true;
+        canvaFinal.enabled = false;
         player = GameObject.FindWithTag("Player");
+        updateScore();
     }
 
     void Update()
@@ -41,11 +57,18 @@ public class GameManager : MonoBehaviour
 
     }
 
+
+
     public void tacada()
     {
         tacadas++;
         pontuacao = tacadas - par;
         lastPos = player.transform.position;
+        updateScore();
+    }
+
+    void updateScore()
+    {
         textPlacar.text = "Par: " + par + "\nTacadas: " + tacadas;
     }
 
@@ -58,42 +81,44 @@ public class GameManager : MonoBehaviour
         }
         canvaPadrao.enabled = false;
         canvaFinal.enabled = true;
-        String p = "";
+        string p = "";
 
         switch (pontuacao)
         {
             case -3:
                 p = "Albatross";
-                passaro.color = new Color32(0, 249, 255, 0);
+                passaro.color = new Color32(0, 249, 255, 255);
                 break;
             case -2:
                 p = "Eagle";
-                passaro.color = new Color32(0, 199, 45, 0);
+                passaro.color = new Color32(0, 199, 45, 255);
                 break;
             case -1:
                 p = "Birdie";
-                passaro.color = new Color32(175, 255, 0, 0);
+                passaro.color = new Color32(175, 255, 0, 255);
                 break;
             case 0:
                 p = "Par";
-                passaro.color = new Color32(255, 254, 0, 0);
+                passaro.color = new Color32(255, 254, 0, 255);
                 break;
             case 1:
                 p = "Bogey";
-                passaro.color = new Color32(255, 186, 0, 0);
+                passaro.color = new Color32(255, 186, 0, 255);
                 break;
             case 2:
                 p = "Double Bogey";
-                passaro.color = new Color32(255, 109, 0, 0);
+                passaro.color = new Color32(255, 109, 0, 255);
                 break;
             case 3:
                 p = "Triple Bogey";
-                passaro.color = new Color32(255, 0, 0, 0);
+                passaro.color = new Color32(255, 0, 0, 255);
                 break;
         }
         textPontuacao.text = "Pontuação: "+pontuacao;
         recorde.text = "Recorde: "+recordeSalvo;
         passaro.text = p;
+        camera1.SetActive(false);
+        camera2.SetActive(true);
     }
 
     public void subirFase()
